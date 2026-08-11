e27053870ba5f2e1b58f6126511e964d4814dcac
Emad Ghasemi <emadghasemi_2006@yahoo.com>
2026-08-11 19:58:24 +0330

    feat: wire native settings custom manager about and updater modules

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 #![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
 
+mod custom;
 mod data;
 mod model;
-mod renderer;
+mod settings;
+mod theme;
 
 #[cfg(target_os = "windows")]
+mod about;
+#[cfg(target_os = "windows")]
 mod app;
+#[cfg(target_os = "windows")]
+mod manager;
+#[cfg(target_os = "windows")]
+mod renderer;
+#[cfg(target_os = "windows")]
+mod update;
 
 #[cfg(target_os = "windows")]
 fn main() {
